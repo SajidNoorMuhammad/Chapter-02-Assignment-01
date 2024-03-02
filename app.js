13c6d520d2d75538639070b96c63003eb198200a
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-03 01:30:54 +0500

    Add Work.

---
+++ app.js
@@ -0,0 +1,46 @@
+// Chapter 02
+        // Question # 01
+        //   var Username = "Username";
+        //   alert (Username);
+
+        // Question # 02
+        // var myName = "Sajid Noor Muhammad"
+        // alert (myName)
+
+        // Question # 03 Part a
+        // var JS = "message"
+        // alert (JS);
+
+        // Question # 03 Part b
+        // var message = "Hello World"
+        // alert (message);
+
+        // Question # 03 Part c
+        // var message = "A communication, usually brief, from one person or group to another."
+        // alert (message);
+
+        // Question # 04
+        var name = "Sajid Noor Muhammad"
+        alert (name);
+
+        var age = "18 Years Old"
+        alert (age);
+
+        var msg = "Certified Mobile Application Development"
+        alert (msg);
+
+        // Question # 05
+        var msg = "PIZZA \nPIZZ \nPIZ \nPI \nP "
+        alert (msg);
+
+        // Question # 06
+        var email = "My email address is snoormuhammad37@gmail.com"
+        alert (email);
+
+        // Question # 07
+        // var book = "I am trying to learn from the Book A smarter \nway to learn JavaScript."
+        // alert (book)
+
+        // Question # 09
+        // var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+        // alert (design);
